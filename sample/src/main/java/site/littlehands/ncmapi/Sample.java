dfb06f85f512dbd17fe44c5627df66e02f1b0cde
moeshin <764686875@qq.com>
2019-06-30 08:20:59 +0800

    feat(sample): add search sample

--- sample/src/main/java/site/littlehands/ncmapi/Sample.java
+++ sample/src/main/java/site/littlehands/ncmapi/Sample.java
@@ -2,7 +2,15 @@ package site.littlehands.ncmapi;
 
 public class Sample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        search();
+    }
 
+    private static void print(String str) {
+        System.out.printf("%s\n", str);
+    }
+
+    private static void search() throws Exception {
+        print(NCMAPI.search("Hope"));
     }
 }
